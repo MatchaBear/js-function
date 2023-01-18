a2e087192e810682765fd20af2b8699f976ea87d
Bernhard Hustomo <bernhard.hustomo@gmail.com>
2023-01-18 19:48:24 +0800

    prettier on js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const operator = prompt('Enter operator (either '+', '-','*','/'');
-const firstNumber = prompt('Enter first number');
-const secondNumber = prompt('Enter second number');
+const operator = prompt("Enter operator (either " + ", " - ("," * ",") / "");
+const firstNumber = prompt("Enter first number");
+const secondNumber = prompt("Enter second number");
 
-function calcApp(firstNumber,secondNumber) {
-if (operator=='+') {
-    return result = firstNumber+secondNumber;
-}
-else if (operator=='-') {
-    return result = firstNumber-secondNumber;
-}
-else if (operator=='*') {
-    return result = firstNumber*secondNumber;
-}
-else if (operator=='/') {
-    return result = firstNumber/secondNumber;
-}
-else {
-    return window.alert("Please enter correct operator")
-}
+function calcApp(firstNumber, secondNumber) {
+  if (operator == "+") {
+    return (result = firstNumber + secondNumber);
+  } else if (operator == "-") {
+    return (result = firstNumber - secondNumber);
+  } else if (operator == "*") {
+    return (result = firstNumber * secondNumber);
+  } else if (operator == "/") {
+    return (result = firstNumber / secondNumber);
+  } else {
+    return window.alert("Please enter correct operator");
+  }
 }
